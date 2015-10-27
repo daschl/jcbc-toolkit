469e7909d7cc6eff8ead60ff8f8f74ca91b4284e
Michael Nitschinger <michael@nitschinger.at>
2015-10-27 22:37:20 +0100

    rustfmt

--- src/analyze/multi_env.rs
+++ src/analyze/multi_env.rs
@@ -2,41 +2,39 @@ use regex::Regex;
 use analyze::Analyzer;
 
 pub struct MultiEnvAnalyzer {
-	occurences: u8,
-	max_envs_found: u8
+    occurences: u8,
+    max_envs_found: u8,
 }
 
 impl MultiEnvAnalyzer {
 
-	pub fn new() -> MultiEnvAnalyzer {
-		MultiEnvAnalyzer { max_envs_found: 0, occurences: 0 }
-	}
+    pub fn new() -> MultiEnvAnalyzer {
+        MultiEnvAnalyzer { max_envs_found: 0, occurences: 0 }
+    }
 
 }
 
 impl Analyzer for MultiEnvAnalyzer {
 
-	fn parse(&mut self, line: &str)  {
-		let re = Regex::new(r"More than 1 Couchbase Environments found \((\d+)\)").unwrap();
-		
-		if re.is_match(line) {
-			self.occurences = self.occurences + 1;
+    fn parse(&mut self, line: &str) {
+        let re = Regex::new(r"More than 1 Couchbase Environments found \((\d+)\)").unwrap();
 
-			let caps = re.captures(line).unwrap();
-			let num_found = caps.at(1).unwrap().parse::<u8>().unwrap();
+        if re.is_match(line) {
+            self.occurences = self.occurences + 1;
 
-			if num_found > self.max_envs_found {
-				self.max_envs_found = num_found;
-			}
-		}
-	}
+            let caps = re.captures(line).unwrap();
+            let num_found = caps.at(1).unwrap().parse::<u8>().unwrap();
 
-	fn print_results(&self) {
-		println!(
-			"[MultiEnvAnalyzer]: {} Occurences, {} Max Environments Reported", 
-			self.occurences, 
-			self.max_envs_found
-		);
-	}
+            if num_found > self.max_envs_found {
+                self.max_envs_found = num_found;
+            }
+        }
+    }
+
+    fn print_results(&self) {
+        println!("[MultiEnvAnalyzer]: {} Occurences, {} Max Environments Reported",
+                 self.occurences,
+                 self.max_envs_found);
+    }
 
 }
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -3,33 +3,35 @@ use std::io::Result;
 use analyze::Analyzer;
 
 pub struct Parser {
-	analyzers: Vec<Box<Analyzer>>
+    analyzers: Vec<Box<Analyzer>>,
 }
 
 impl Parser {
 
-	pub fn new() -> Parser {
-		Parser { analyzers: Vec::new() }
-	}
+    pub fn new() -> Parser {
+        Parser { analyzers: Vec::new() }
+    }
 
-	pub fn register_analyzer(&mut self, analyzer: Box<Analyzer>) {
-		self.analyzers.push(analyzer);
-	}
+    pub fn register_analyzer(&mut self, analyzer: Box<Analyzer>) {
+        self.analyzers.push(analyzer);
+    }
 
-	pub fn parse<I>(&mut self, iter: I) where I: Iterator<Item=Result<String>> {
-		for i in iter {
-			let line = i.ok().unwrap();
+    pub fn parse<I>(&mut self, iter: I)
+        where I: Iterator<Item = Result<String>>
+    {
+        for i in iter {
+            let line = i.ok().unwrap();
 
-			for a in &mut self.analyzers {
-				a.parse(&line);
-			}
-		}
-	}
+            for a in &mut self.analyzers {
+                a.parse(&line);
+            }
+        }
+    }
 
-	pub fn print_results(&self) {
-		for a in &self.analyzers {
-			a.print_results();
-		}
-	}
+    pub fn print_results(&self) {
+        for a in &self.analyzers {
+            a.print_results();
+        }
+    }
 
 }
